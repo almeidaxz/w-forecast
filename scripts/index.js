9c168cae09eb35cf86bd072b17dc069984a48373
almeidaxz <almeidaxz@live.com>
2022-10-21 12:21:12 -0300

    implement get coordinates from browser

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,19 @@ import { changeToCelc, changeToFahr } from "../scripts/utils/utils.js";
 const toggleState = document.querySelector('#toggle-state');
 const selectedMetric = document.querySelector('#selected-metric');
 
+let latitude, longitude;
+
+(() => {
+    window.navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude: lat, longitude: long } = position.coords;
+
+        latitude = lat;
+        longitude = long;
+    });
+})()
+
 toggleState.addEventListener('click', (e) => {
+    console.log(latitude, longitude);
     if (!e.target.checked) {
         changeToFahr(selectedMetric);
     }
