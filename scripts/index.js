1fb8233ce29d685e5c4f22a86f1881f126c23b6e
almeidaxz <almeidaxz@live.com>
2022-10-24 12:37:44 -0300

    start show results implementation

--- scripts/index.js
+++ scripts/index.js
@@ -7,14 +7,43 @@ const metricState = document.querySelector('#metric-state');
 const selectedMetric = document.querySelector('#selected-metric');
 const locationInput = document.querySelector('#location-input');
 const optionsWrapper = document.querySelector('.options-wrapper');
+const weatherContentWrapper = document.querySelector('#weather-content-wrapper');
 
 let latitude, longitude;
 
+const getCurrentWeather = async () => {
+    const response = await fetch(`${weather}/${latitude},${longitude}/next5days?key=${weatherKey}`);
+    const weathers = await response.json();
+    console.log(weathers);
+
+    if(!weathers.days.length) return;
+
+    weathers.days.map((day) => {
+        console.log(day);
+        const dayWeather = document.createElement('div');
+        dayWeather.textContent = day.temp;
+        dayWeather.classList.add('day-weather');
+        weatherContentWrapper.append(dayWeather);
+    });
+}
+
 (() => {
+    const localLat = localStorage.getItem('latitude');
+    const localLong = localStorage.getItem('longitude');
+
+    if(localLat && localLong) {
+        latitude = localLat;
+        longitude = localLong;
+        getCurrentWeather();
+        return;
+    }
+
     window.navigator.geolocation.getCurrentPosition(async (position) => {
         const { latitude: lat, longitude: long } = position.coords;
         latitude = lat;
         longitude = long;
+        localStorage.setItem('latitude', lat);
+        localStorage.setItem('longitude', long);
 
         const response = await fetch(`${geolocationByCoods}${lat},${long}&key=${googleKey}`);
         const address = await response.json();
@@ -39,28 +68,35 @@ metricState.addEventListener('click', (e) => {
 });
 
 const getAddress = async (e) => {
+    if (e.target.value === '' || e.target.value.trim() === '') {
+        optionsWrapper.classList.add('hidden');
+        return;
+    };
+
+    optionsWrapper.classList.remove('hidden');
     const currentValue = e.target.value;
     const response = await fetch(`${geolocationByAddress}${currentValue}&key=${googleKey}`);
     const searchByAddress = await response.json();
 
-    searchByAddress.results.map((result) => {
+    if (!searchByAddress.results.length) return;
+
+    const option = document.querySelector('.option');
+
+    if (!option) {
         const option = document.createElement('span');
-        option.textContent = result.formatted_address;
+        option.textContent = searchByAddress.results[0].formatted_address;
         option.classList.add('option')
         optionsWrapper.append(option);
-    });
-}
 
+        option.addEventListener('click', (e) => {
+            locationInput.value = e.target.textContent;
+            option.remove();
+        });
+        return;
+    }
+
+    option.textContent = searchByAddress.results[0].formatted_address;
+}
 const handleFetchAddress = _.debounce(getAddress, 500);
 
-locationInput.addEventListener('keydown', (e) =>  handleFetchAddress(e));
-
-if (latitude && longitude) {
-    getCurrentWeather();
-}
-
-const getCurrentWeather = async () => {
-    const response = await fetch(`${weather}/${latitude},${longitude}/next5days?key=${weatherKey}`);
-    const weathers = await response.json();
-    console.log(weathers);
-}
+locationInput.addEventListener('keydown', (e) => handleFetchAddress(e));
